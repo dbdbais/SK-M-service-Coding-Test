e34c52a8e8f73f2983bb738fb52d04a6d0668434
Kangwoo Lee <rain5191@naver.com>
2024-12-03 23:12:27 +0900

    init: SecurityConfig, Gradle 설정

---
+++ task/src/main/java/org/sk/task/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package org.sk.task.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * H2 DB를 열기 위해 Security에서 제외
+     * @return
+     */
+    @Bean
+    @ConditionalOnProperty(name = "spring.h2.console.enabled",havingValue = "true")
+    public WebSecurityCustomizer configureH2ConsoleEnable() {
+        return web -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console());
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf((auth) -> auth.disable());
+
+        http
+                .formLogin((auth) -> auth.disable());
+
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/h2-console/**").permitAll()  // H2 콘솔 경로 허용
+                        .requestMatchers("api/v1/user/**").permitAll()
+                        .requestMatchers("/api/v1/board/**").permitAll() // 변경된 부분
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+
+}
