3575950dcb6ece27b4b8d0515f84958c76af4010
Kangwoo Lee <rain5191@naver.com>
2024-12-03 23:16:07 +0900

    feat: Listener 설정

--- task/src/main/java/org/sk/task/TaskApplication.java
+++ task/src/main/java/org/sk/task/TaskApplication.java
@@ -1,9 +1,12 @@
 package org.sk.task;
 
+import jakarta.persistence.EntityListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TaskApplication {
 
 	public static void main(String[] args) {
