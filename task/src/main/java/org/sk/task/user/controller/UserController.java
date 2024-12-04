e2985d5bfa9e81b294ea070b9b9e43008fcb5bb3
Kangwoo Lee <rain5191@naver.com>
2024-12-04 20:22:27 +0900

    feat: 로그인, 로그아웃 비즈니스 로직에 세션 관리 사항 반영

--- task/src/main/java/org/sk/task/user/controller/UserController.java
+++ task/src/main/java/org/sk/task/user/controller/UserController.java
@@ -3,8 +3,7 @@ package org.sk.task.user.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.sk.task.common.ResponseDto;
 import org.sk.task.common.code.StatusCode;
-import org.sk.task.user.dto.UserLoginDto;
-import org.sk.task.user.dto.UserRegisterDto;
+import org.sk.task.user.dto.*;
 import org.sk.task.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -29,12 +28,37 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity<ResponseDto> login(@RequestBody UserLoginDto userLoginDto){
-        return ResponseDto.response(userService.loginUser(userLoginDto));
+        UserLoginResponseDto userLoginResponseDto = userService.loginUser(userLoginDto);
+        if (userLoginResponseDto.getCookie() != null) {
+            return ResponseEntity
+                    .status(userLoginResponseDto.getStatusCode().getHttpStatus())
+                    .header("Set-Cookie", userLoginResponseDto.getCookie().toString()) // 쿠키를 응답 헤더에 추가
+                    .body(new ResponseDto(userLoginResponseDto.getStatusCode())); // ResponseDto 객체를 본문으로 설정
+        } else {
+            return ResponseEntity
+                    .status(userLoginResponseDto.getStatusCode().getHttpStatus())
+                    .body(new ResponseDto(userLoginResponseDto.getStatusCode())); // 상태 코드만 응답 본문에 포함
+        }
+    }
+
+    @DeleteMapping("/logout")
+    public ResponseEntity<ResponseDto> logout(@CookieValue("SESSION_ID") String sessionId){
+        UserLogoutResponseDto userLogoutResponseDto = userService.logoutUser(sessionId);
+        if (userLogoutResponseDto.getCookie() != null) {
+            return ResponseEntity
+                    .status(userLogoutResponseDto.getStatusCode().getHttpStatus())
+                    .header("Set-Cookie", userLogoutResponseDto.getCookie().toString()) // 쿠키를 응답 헤더에 추가
+                    .body(new ResponseDto(userLogoutResponseDto.getStatusCode())); // ResponseDto 객체를 본문으로 설정
+        } else {
+            return ResponseEntity
+                    .status(userLogoutResponseDto.getStatusCode().getHttpStatus())
+                    .body(new ResponseDto(userLogoutResponseDto.getStatusCode())); // 상태 코드만 응답 본문에 포함
+        }
     }
 
     @GetMapping("/list/{userId}")
-    public ResponseEntity<ResponseDto> getWritten(@PathVariable Long userId){
-        return ResponseDto.response(StatusCode.SUCCESS,userService.getWrittenBoard(userId));
+    public ResponseEntity<ResponseDto> getWritten(@CookieValue("SESSION_ID") String sessionId){
+        return ResponseDto.response(StatusCode.SUCCESS,userService.getWrittenBoard(sessionId));
     }
 
 
--- task/src/main/java/org/sk/task/user/service/UserService.java
+++ task/src/main/java/org/sk/task/user/service/UserService.java
@@ -5,16 +5,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.sk.task.board.dto.BoardSpecDto;
 import org.sk.task.board.entity.Board;
 import org.sk.task.common.code.StatusCode;
-import org.sk.task.user.dto.UserLoginDto;
-import org.sk.task.user.dto.UserRegisterDto;
+import org.sk.task.session.service.SessionService;
+import org.sk.task.user.dto.*;
 import org.sk.task.user.entity.User;
 import org.sk.task.user.repoisitory.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Transactional
 @Slf4j
@@ -23,11 +25,13 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final SessionService sessionService;
 
     @Autowired
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, SessionService sessionService) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.sessionService = sessionService;
     }
 
     public StatusCode registerUser(UserRegisterDto userRegisterDto){
@@ -47,22 +51,63 @@ public class UserService {
         return StatusCode.SUCCESS;
     }
 
-    public StatusCode loginUser(UserLoginDto userLoginDTO){
+    public UserLoginResponseDto loginUser(UserLoginDto userLoginDTO){
         Optional<User> sUser = userRepository.findByNickname(userLoginDTO.getNickname());
 
         if(sUser.isEmpty()){
-            return StatusCode.NO_USER;
+            return new UserLoginResponseDto(StatusCode.NO_USER);
         }
 
         if(bCryptPasswordEncoder.matches(userLoginDTO.getPassword(),sUser.get().getPassword())){
-            return StatusCode.SUCCESS;
+
+            if(sessionService.isLogin(sUser.get().getUserId())){
+                return new UserLoginResponseDto(StatusCode.ALREADY_LOGIN);
+            }
+            String sessionId = UUID.randomUUID().toString(); // 고유 세션 ID 생성
+            sessionService.createSession(sUser.get().getUserId(),sessionId); // DB에 세션 저장
+            ResponseCookie cookie = ResponseCookie.from("SESSION_ID", sessionId)
+                    .httpOnly(true)
+                    .path("/")
+                    .build();
+
+            return new UserLoginResponseDto(cookie,StatusCode.SUCCESS);
         }
         else{
-            return StatusCode.WRONG_PW;
+            return new UserLoginResponseDto(StatusCode.WRONG_PW);
         }
     }
 
-    public List<BoardSpecDto> getWrittenBoard(Long userId){
+    public UserLogoutResponseDto logoutUser(String sessionIdFromCookie){
+        Long userId = sessionService.getUserIdBySessionId(sessionIdFromCookie);
+
+        Optional<User> sUser = userRepository.findById(userId);
+
+        if(sUser.isEmpty()){
+            return new UserLogoutResponseDto(StatusCode.NO_USER);
+        }
+
+        if (!sessionService.sessionCheck(userId, sessionIdFromCookie)) {
+            return new UserLogoutResponseDto(StatusCode.FORBIDDEN_ACCESS); // 세션 검증 실패
+        }
+
+
+        if(sessionService.isLogin(userId)){
+            sessionService.deleteSession(userId);
+            ResponseCookie cookie = ResponseCookie.from("SESSION_ID", "")
+                    .httpOnly(true)
+                    .path("/")
+                    .maxAge(0) // 쿠키 만료 시간 0으로 설정
+                    .build();
+
+            return new UserLogoutResponseDto(cookie,StatusCode.SUCCESS);
+        }
+        else{
+            return new UserLogoutResponseDto(StatusCode.BAD_REQUEST);
+        }
+    }
+
+    public List<BoardSpecDto> getWrittenBoard(String sessionIdFromCookie){
+        Long userId = sessionService.getUserIdBySessionId(sessionIdFromCookie);
         Optional<User> sUser = userRepository.findById(userId);
 
         if(sUser.isEmpty()){
